351e9ee70e151d645ca356ab10efa925625b1cd9
Sergey Blednov <sergey.blednov@pixacore.com>
2017-04-10 14:52:18 -0400

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void OnEnable()
+	{
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
+
+	void OnDisable() 
+	{
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;		
+	}
+
+	void OnLevelFinishedLoading (Scene scene, LoadSceneMode mode)
+	{
+		Debug.Log ("Loaded level: " + SceneManager.GetActiveScene().buildIndex);
+		if (SceneManager.GetActiveScene().buildIndex == 0) {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		SceneManager.LoadScene (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel() 
+	{
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start() {
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnLevelWasLoaded (int level)
+	{
+		AudioClip clip = levelMusicChangeArray [level];
+		if (clip) {
+			audioSource.clip = clip;
+			audioSource.loop = true;
+			audioSource.Play ();
+		}
+	}
+}
