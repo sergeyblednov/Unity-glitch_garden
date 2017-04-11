36da33943723e1e7bec13f41b7d4fb8afc70f96a
Sergey Blednov <sergey.blednov@pixacore.com>
2017-04-11 18:28:20 -0400

    Added triggers

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+	[Range(-1f, 1.5f)]
+	public float currentSpeed;
+
+	void Start () 
+	{
+		Rigidbody2D rigitbody = gameObject.AddComponent<Rigidbody2D> ();
+		rigitbody.bodyType = RigidbodyType2D.Kinematic;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		Debug.Log (name + " trigger enter");
+	}
+
+	public void SetSpeed(float speed) 
+	{
+		currentSpeed = speed;
+	}
+}
---
+++ Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartVolume : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		MusicManager manager = GameObject.FindObjectOfType<MusicManager> ();
+		manager.SetVolume (PlayerPrefsManager.GetMasterVolume ());
+	}
+}
