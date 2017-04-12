fe2fcc201261c81b741ea68c29c734d78079a7c6
Sergey Blednov <sergey.blednov@pixacore.com>
2017-04-12 15:02:15 -0400

    Animate firing projectiles

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject projectile;
+	public GameObject projectileParent;
+	public GameObject gun;
+
+	private void Fire ()
+	{
+		GameObject newProjectile = Instantiate (projectile);
+		newProjectile.transform.parent = projectileParent.transform;
+		newProjectile.transform.position = gun.transform.position;
+	}
+}
